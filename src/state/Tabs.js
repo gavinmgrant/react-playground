143bd14b992362cd83c9b65997f080f107ac9762
Gavin Grant <gavingrant@gmail.com>
2020-07-01 15:19:25 -0700

    finished tab component

--- src/state/Tabs.js
+++ src/state/Tabs.js
@@ -1,18 +1,26 @@
 import React from "react";
 
 class Tabs extends React.Component {
-    static defaultProps = { tabs: [] };
+    static defaultProps = { tabs: [], 
+    };
+    state = {
+        currentTabIndex: 0
+    };
+
+    handleButtonClick(index) {
+        this.setState({ currentTabIndex: index })
+    }
 
     renderButtons() {
         return this.props.tabs.map((tab, index) => (
-            <button key={index}>
+            <button key={index} onClick={() => this.handleButtonClick(index)}>
                 {tab.name}
             </button>
         ))
     }
 
     renderContent() {
-        const currentTab = this.props.tabs[0]
+        const currentTab = this.props.tabs[this.state.currentTabIndex]
         return (
             <div className='content'>
                 {currentTab.content}
@@ -24,7 +32,7 @@ class Tabs extends React.Component {
         return (
             <div>
                 {this.renderButtons()}
-                {this.props.tabs.length && this.renderContent()}
+                {!!this.props.tabs.length && this.renderContent()}
             </div>
         )
     }
