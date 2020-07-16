1e234af8c4c1d0b052f7e058cffd53c1179091aa
Gavin Grant <gavingrant@gmail.com>
2020-07-16 16:13:49 -0700

    added ability to update deeply nested context using provider

--- src/lang-context/LangControls.js
+++ src/lang-context/LangControls.js
@@ -10,24 +10,28 @@ export default function LangControls(props) {
                 return (
                     <>
                         <button
+                            onClick={() => props.onSetLang('en-GB')}
                             disabled={value.lang === 'en-GB'}>
                             British{' '}
                             <span role='img' aria-label='en-GB'>GB</span>
                         </button>
                         {' '}
                         <button
+                            onClick={() => props.onSetLang('en-US')}
                             disabled={value.lang === 'en-US'}>
                             American{' '}
                             <span role='img' aria-label='en-US'>US</span>
                         </button>
                         {' '}
                         <button
+                            onClick={() => props.onSetLang('in')}
                             disabled={value.lang === 'in'}>
                             Indonesian{' '}
                             <span role='img' aria-label='in'>IN</span>
                         </button>
                         {' '}
                         <button
+                            onClick={() => props.onSetLang('ko')}
                             disabled={value.lang === 'ko'}>
                             Korean{' '}
                             <span role='img' aria-label='ko'>KR</span>
