151078a01e291a7d1f1dff3638979f6669182aef
Gavin Grant <gavingrant@gmail.com>
2020-07-16 12:34:57 -0700

    added the first iteration of the language app teaching the concept of context in React

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import App from './paltrows-power-toes/App';
+import AppLang from './lang-context/AppLang';
 import './index.css';
 
 ReactDOM.render(
-    <BrowserRouter>
-        <App /> 
-    </BrowserRouter>,
+    <AppLang />,
     document.getElementById('root')
 );
---
+++ src/lang-context/LangControls.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import LanguageContext from './LanguageContext'
+
+console.log(LanguageContext)
+
+export default function LangControls(props) {
+    return (
+        <LanguageContext.Consumer>
+            {(value) => {    
+                return (
+                    <>
+                        <button
+                            disabled={value.lang === 'en-GB'}>
+                            British{' '}
+                            <span role='img' aria-label='en-GB'>GB</span>
+                        </button>
+                        {' '}
+                        <button
+                            disabled={value.lang === 'en-US'}>
+                            American{' '}
+                            <span role='img' aria-label='en-US'>US</span>
+                        </button>
+                        {' '}
+                        <button
+                            disabled={value.lang === 'in'}>
+                            Indonesian{' '}
+                            <span role='img' aria-label='in'>IN</span>
+                        </button>
+                        {' '}
+                        <button
+                            disabled={value.lang === 'ko'}>
+                            Korean{' '}
+                            <span role='img' aria-label='ko'>KR</span>
+                        </button>
+                    </>
+                )
+            }}
+        </LanguageContext.Consumer>
+    );
+}
